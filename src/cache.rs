de15c28055e3627ddddb024459bb76ab1ca8694b
Honey2339 <prasoon2study@gmail.com>
2025-08-19 23:25:20 +0530

    feat : added cache and repo

---
+++ src/cache.rs
@@ -0,0 +1,25 @@
+use std::{fs, io, path::PathBuf};
+
+use dirs::{cache_dir, home_dir};
+
+
+fn get_cache_root() -> PathBuf {
+    cache_dir()
+    .unwrap_or_else(|| home_dir().unwrap_or_else(|| PathBuf::from("./config")))
+    .join("nay")
+    .join("builds")
+}
+
+pub fn get_package_cache(pkg: &str) -> io::Result<PathBuf> {
+    let root = get_cache_root();
+    let pkg_dir = root.join(pkg);
+
+    if !pkg_dir.exists() {
+        fs::create_dir_all(&pkg_dir)?;
+        println!("Created cache dir : {}", pkg_dir.display());
+    } else {
+        println!("Using existing cache : {}", pkg_dir.display());
+    }
+
+    Ok(pkg_dir)
+}
--- src/install.rs
+++ src/install.rs
@@ -1,43 +1,17 @@
-use std::path::PathBuf;
-use std::process::Command;
-use std::{fs, io};
+use crate::{cache::get_package_cache, repo::check_official_package};
+use std::io;
 
-use dirs::{cache_dir, home_dir};
+pub fn install_package(pkg: &str) -> io::Result<()> {
+    println!("Checking if `{}` exists in repo", pkg);
 
-fn get_cache_root() -> PathBuf {
-    cache_dir()
-    .unwrap_or_else(|| home_dir().unwrap_or_else(|| PathBuf::from(".")))
-    .join("nay")
-    .join("builds")
-}
-
-pub fn install_package(pkg: &str) -> io::Result<()>{
-    println!("Checking if `{}` exists in repo or not", pkg);
-
-    let status = Command::new("pacman")
-        .args(["-Si", pkg])
-        .status()
-        .map_err(|e|{
-            io::Error::new(
-                io::ErrorKind::Other,
-                format!("Failed to run pacman: {}", e),
-            )
-        });
-
-    if status.is_ok(){
-        println!("{}, found official pkg", pkg);
+    if check_official_package(pkg)? {
+        println!("{} found in official repo", pkg);
         return Ok(())
     }
 
-    let cache_root = get_cache_root();
-    let pkg_cache = cache_root.join(pkg);
+    println!("{} not in repo, falling back to AUR", pkg);
 
-    if pkg_cache.exists(){
-        println!("Using existing cache at {}", pkg_cache.display());
-    } else {
-        println!("Creating cache dir: {}", pkg_cache.display());
-        fs::create_dir_all(&pkg_cache);
-    }
+    let _cache = get_package_cache(pkg)?;
 
     Ok(())
 }
